66fe96e63bb28facaba7b4810e469fd996fc8b6b
Florian Pichler <f.pichler@anfe.ma>
2018-02-12 18:27:47 +0100

    FEATURE: Move over transforms from ember-luxon fork

---
+++ tests/unit/transforms/luxon-duration-test.js
@@ -0,0 +1,9 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('transform:luxon-duration', 'Unit | Transform | luxon duration', {});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let transform = this.subject();
+  assert.ok(transform);
+});
---
+++ tests/unit/transforms/luxon-interval-test.js
@@ -0,0 +1,9 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('transform:luxon-interval', 'Unit | Transform | luxon interval', {});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let transform = this.subject();
+  assert.ok(transform);
+});
